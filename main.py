db61dc4a0ca61ee044707eae5d59ac606c8a88c7
thebigbeanest <tannergilliambusiness@gmail.com>
2024-07-10 08:48:15 -0400

    Pushing up homework

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
 import random
 import json
-characters = []
+from smash import Character, Battle
 
-with open('characters.json') as json_file:
-    characters = json.load(json_file)
+def load_characters(filename):
+    with open(filename) as json_file:
+        data = json.load(json_file)
+    characters = []
+    for char in data:
+        name = char['name']
+        attacks = char['attacks']
+        # Assuming each character starts with 100 health
+        character = Character(name, 100, attacks)
+        characters.append(character)
+    return characters
+
+def select_character(characters):
+    print("Select your character:")
+    for i, character in enumerate(characters):
+        print(f"{i+1}. {character.name}")
+    choice = input("Enter the number of your character: ")
+    if choice.isdigit() and 1 <= int(choice) <= len(characters):
+        return characters[int(choice) - 1]
+    else:
+        print("Invalid choice. A random character will be chosen for you.")
+        return random.choice(characters)
+
+def game():
+    characters = load_characters('characters.json')
+    user_character = select_character(characters)
+    opponent_character = random.choice([char for char in characters if char != user_character])
+    
+    print(f"You chose {user_character.name}. Your opponent is {opponent_character.name}.")
+    
+    battle = Battle(user_character, opponent_character)
+    winner = battle.fight()
+    
+    print(f"The winner is {winner}!")
+
+if __name__ == "__main__":
+    game()
--- smash.py
+++ smash.py
@@ -1,1 +1,39 @@
 import random
+
+class Character:
+    def __init__(self, name, health, attacks):
+        self.name = name
+        self.health = health
+        self.attacks = attacks
+
+    def decrement_health(self, damage):
+        self.health -= damage
+
+    def random_attack(self):
+        return random.choice(self.attacks)
+
+class Battle:
+    def __init__(self, character1, character2):
+        self.character1 = character1
+        self.character2 = character2
+
+    def fight(self):
+        while self.character1.health > 0 and self.character2.health > 0:
+            attack1 = self.character1.random_attack()
+            self.character2.decrement_health(attack1['damage'])
+            print(f"{self.character1.name} attacks {self.character2.name} with {attack1['name']} for {attack1['damage']} damage.")
+            if self.character2.health <= 0:
+                print(f"{self.character2.name} has been defeated!")
+                break
+
+            attack2 = self.character2.random_attack()
+            self.character1.decrement_health(attack2['damage'])
+            print(f"{self.character2.name} attacks {self.character1.name} with {attack2['name']} for {attack2['damage']} damage.")
+            if self.character1.health <= 0:
+                print(f"{self.character1.name} has been defeated!")
+                break
+
+        if self.character1.health > 0:
+            return self.character1.name
+        else:
+            return self.character2.name
